7e733a5205df4c0b6712c869aab8a76d70aac676
ccb_haha <ccb12011@163.com>
2018-06-20 01:09:32 +0800

    44_陈成标_第一课作业
    @fantasyrex3

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,53 @@
-/*作业请提交在这个目录下*/
+/*我的作业*/
+/*作业*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    
+    address employer;
+    address employee;
+    uint salary;
+    uint lastPayday;
+    uint constant payDuration = 10 seconds;
+    
+    function setEmployer() {
+        if(employer == 0x0) {
+            employer = msg.sender;
+        }
+    }
+    
+    function updateEmployee(address add, uint s, uint startPayday) {
+        if(employer == msg.sender) {
+            employee = add;
+            salary = s * 1 ether;
+            lastPayday = startPayday;
+        }
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    } 
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return this.balance >= salary;
+    }
+    
+    function getPay() payable{
+        if(msg.sender != employee) {
+            revert();    
+        }
+        
+        uint nextPayday = lastPayday + payDuration;
+        if(nextPayday > now) {
+            revert();
+        }
+        
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+    
+}
