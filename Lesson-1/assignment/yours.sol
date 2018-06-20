6512e346ee178c7f823f174f83ea70b7f3f22643
ccb_haha <ccb12011@163.com>
2018-06-20 21:03:31 +0800

    44_陈成标_第一课作业_v2
    @fantasyrex3
    智能合约第一课作业优化
    1.public修饰可查询变量
    2.考虑注释
    3.结算上一个员工工资
    4.一次性结算未结工资
    5.增加单独修改工资的函数
    6.检查输入校验require(!=)
    7.考虑余额不足的情况
    8.检查salary单位
    9.函数声明view，表示不修改状态

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,53 +1,82 @@
 /*我的作业*/
-/*作业*/
 pragma solidity ^0.4.14;
 
 contract Payroll {
     
-    address employer;
-    address employee;
-    uint salary;
+    address employer;//can't be queried
+    address public employee;//'public' can be queried
+    uint public salary;
     uint lastPayday;
     uint constant payDuration = 10 seconds;
     
+    //set owner address
     function setEmployer() {
-        if(employer == 0x0) {
-            employer = msg.sender;
-        }
+        require(employer == 0x0);//check the employer address
+        employer = msg.sender;
     }
     
-    function updateEmployee(address add, uint s, uint startPayday) {
-        if(employer == msg.sender) {
-            employee = add;
-            salary = s * 1 ether;
-            lastPayday = startPayday;
+    //update employee address, salary
+    function updateEmployee(address add, uint s) {
+        require(employer == msg.sender);//check the employer address
+        
+        if(employee != 0x0){
+            uint payment = salary * (now - lastPayday)/payDuration;
+            require(this.balance > payment);//check balance
+            employee.transfer(payment);
         }
+        
+        employee = add;
+        salary = s * 1 ether;
+        lastPayday = now;
     }
     
+    // update salary
+    function updateSalary(uint s){
+        require(employer == msg.sender);
+        require(employee != 0x0);
+        salary = s * 1 ether;
+    }
+    
+    //deposit ether
     function addFund() payable returns (uint) {
         return this.balance;
     } 
     
-    function calculateRunway() returns (uint) {
+    //calculate the contract can pay how many times for the employee
+    function calculateRunway() view returns (uint) {
         return this.balance / salary;
     }
     
-    function hasEnoughFund() returns (bool) {
+    //check the contract balance
+    function hasEnoughFund() view returns (bool) {
         return this.balance >= salary;
     }
     
-    function getPay() payable{
-        if(msg.sender != employee) {
-            revert();    
-        }
+    //employee get salary by himself
+    function getPaid() payable public{
+        require(msg.sender == employee);
+        require(this.balance >= salary);
         
         uint nextPayday = lastPayday + payDuration;
         if(nextPayday > now) {
             revert();
         }
-        
+
         lastPayday = nextPayday;
         employee.transfer(salary);
+    }
+    
+    //get all salary in one time
+    function getAllPaid() payable{
+        require(msg.sender == employee);
+        uint time = now;//keey the time
+        uint times = (time - lastPayday)/payDuration;
+        uint payment = salary * times;
+        
+        require(this.balance > payment);//check balance
+        
+        lastPayday = time - times * payDuration;
+        employee.transfer(payment);
     }
     
 }
