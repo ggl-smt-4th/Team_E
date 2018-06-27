c1ad013eeb286961b01abf7e08f8f196280aeb68
ccb_haha <ccb12011@163.com>
2018-06-27 22:34:39 +0800

    第三次作业

--- projects/lesson-3/contracts/Payroll.sol
+++ projects/lesson-3/contracts/Payroll.sol
@@ -13,52 +13,68 @@ contract Payroll is Ownable {
         uint lastPayday;
     }
     
-    mapping (address => Employee) employees;
+    mapping (address => Employee) public employees;
     
-    //uint constant payDuration = 30 days;
-    uint constant payDuration = 10 seconds;
+    uint constant payDuration = 30 days;
+    //uint constant payDuration = 10 seconds;
     uint public totalSalary = 0;
+    
+    modifier checkInputAddressExist(address employeeId) {
+        Employee employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        _;
+    }
+    
+    modifier checkInputAddressNoError(address employeeId){
+        assert(employeeId != 0x0);
+        _;
+    }
+    
+    modifier checkSalary(uint s){
+        assert(s > 0 && s < uint(-1));//learn from classmate
+        _;
+    }
     
     function _partialPaid(Employee employee) private {
         uint payment = employee.salary.mul(now.sub(employee.lastPayday)).div(payDuration);
         employee.id.transfer(payment);
     }
     
-    function addEmployee(address employeeId, uint salary) onlyOwner public {
-        var employee = employees[employeeId];
+    function addEmployee(address employeeId, uint salary) onlyOwner checkInputAddressNoError(employeeId) checkSalary(salary) public {
+        Employee employee = employees[employeeId];
         assert(employee.id == 0x0);
         
         uint s = salary.mul(1 ether);
         totalSalary = totalSalary.add(s);
-        employees[employeeId]=Employee(employeeId, s, now);
+        employees[employeeId] = Employee(employeeId, s, now);
     }
 
-    function removeEmployee(address employeeId) onlyOwner public {
-        require(msg.sender == owner);
-        var employee = employees[employeeId];
-        assert(employee.id != 0x0);
+    function removeEmployee(address employeeId) onlyOwner checkInputAddressExist(employeeId) checkInputAddressNoError(employeeId) public {
+        Employee employee = employees[employeeId];
         
         totalSalary = totalSalary.sub(employee.salary);
-        Employee tmpEmployee = Employee(employee.id,employee.salary,employee.lastPayday);
+        _partialPaid(employee);
         delete employees[employeeId];
-        _partialPaid(tmpEmployee);//delete the employee first, and then paid for the employee
+        
     }
 
-    function changePaymentAddress(address oldAddress, address newAddress) onlyOwner public {
-        // TODO: your code here
+    function changePaymentAddress(address oldAddress, address newAddress) onlyOwner checkInputAddressExist(oldAddress) checkInputAddressNoError(oldAddress) public {
+        Employee employee = employees[oldAddress];
+        //assert(newAddress != 0x0);
+        
+        //employees[newAddress] = Employee(newAddress,employee.salary,employee.lastPayday);
+        //delete employees[oldAddress];
     }
 
-    function updateEmployee(address employeeId, uint salary) onlyOwner public {
-        require(msg.sender == owner);
-        var employee = employees[employeeId];
-        assert(employee.id != 0x0);
+    function updateEmployee(address employeeId, uint salary) onlyOwner checkInputAddressExist(employeeId) checkInputAddressNoError(employeeId) checkSalary(salary) public {
+        Employee employee = employees[employeeId];
         
         totalSalary = totalSalary.sub(employee.salary).add(salary.mul(1 ether));
-        Employee tmpEmployee = Employee(employee.id,employee.salary,employee.lastPayday);
+        _partialPaid(employee);
         employees[employeeId].salary = salary.mul(1 ether);
         employees[employeeId].lastPayday = now;
         
-        _partialPaid(tmpEmployee);//update the employee first, and then paid for the employee
+        
     }
 
     function addFund() payable public returns (uint) {
@@ -66,21 +82,23 @@ contract Payroll is Ownable {
     }
 
     function calculateRunway() public view returns (uint) {
-        return this.balance.div(totalSalary);
+        assert(totalSalary>0);
+        return address(this).balance.div(totalSalary);
     }
 
     function hasEnoughFund() public view returns (bool) {
         return calculateRunway() > 0;
     }
 
-    function getPaid() public {
-        var employee = employees[msg.sender];
-        assert(employee.id != 0x0);
+    function getPaid() checkInputAddressExist(msg.sender) public {
+        Employee employee = employees[msg.sender];
         
         uint nextPayday = employee.lastPayday.add(payDuration);
         assert(nextPayday < now);
+        assert(employee.salary < address(this).balance);
         
         employees[msg.sender].lastPayday = nextPayday;
         employee.id.transfer(employee.salary);
     }
+    
 }
