38fb385a52e25a594af202a5d7ffb7291424c8f6
Aiden <4264530+owen563@users.noreply.github.com>
2018-07-04 21:54:45 +0800

    Update Payroll.sol

--- projects/lesson-5/contracts/Payroll.sol
+++ projects/lesson-5/contracts/Payroll.sol
@@ -8,12 +8,13 @@ contract Payroll is Ownable {
     /**
      * event will be introduced in lesson 6
      */
+/* 
     event AddFund(address indexed from, uint value);
     event GetPaid(address indexed employee, uint value);
     event AddEmployee(address indexed from, address indexed employee, uint salary);
     event UpdateEmployee(address indexed from, address indexed employee, uint salary);
     event RemoveEmployee(address indexed from, address indexed removed);
-
+*/
     using SafeMath for uint;
 
     /**
@@ -69,7 +70,7 @@ contract Payroll is Ownable {
         employees[employeeId] = Employee(index, salary, now);
 
         totalSalary = totalSalary.add(salary);
-        AddEmployee(msg.sender, employeeId, salary);
+        //AddEmployee(msg.sender, employeeId, salary);
     }
 
     function removeEmployee(address employeeId) public onlyOwner shouldExist(employeeId) {
@@ -79,6 +80,7 @@ contract Payroll is Ownable {
         uint index = employees[employeeId].index;
         totalSalary = totalSalary.sub(salary);
 
+        //employees[employeeId].lastPayday=0;
         delete employees[employeeId];
 
         delete employeeAddressList[index];
@@ -90,7 +92,7 @@ contract Payroll is Ownable {
 
         // adjust length
         employeeAddressList.length -= 1;
-        RemoveEmployee(msg.sender, employeeId);
+       // RemoveEmployee(msg.sender, employeeId);
     }
 
     function changePaymentAddress(address oldAddress, address newAddress) public onlyOwner shouldExist(oldAddress) shouldNotExist(newAddress) {
@@ -110,11 +112,11 @@ contract Payroll is Ownable {
         employees[employeeId].lastPayday = now;
         totalSalary = totalSalary.add(salary).sub(oldSalary);
 
-        UpdateEmployee(msg.sender, employeeId, salary);
+        //UpdateEmployee(msg.sender, employeeId, salary);
     }
 
     function addFund() payable public returns (uint) {
-        AddFund(msg.sender, msg.value);
+        //AddFund(msg.sender, msg.value);
         return address(this).balance;
     }
 
@@ -137,7 +139,7 @@ contract Payroll is Ownable {
 
         employees[employeeId].lastPayday = nextPayday;
         employeeId.transfer(employees[employeeId].salary);
-        GetPaid(msg.sender, employees[employeeId].salary);
+        //GetPaid(msg.sender, employees[employeeId].salary);
     }
 
     function getEmployerInfo() view public returns (uint balance, uint runway, uint employeeCount) {
